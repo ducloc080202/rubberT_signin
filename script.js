7ea2c07c48b836d5b561d844e318b0eeda8e3559
ducloc080202 <ducloc080202@gmail.com>
2022-09-19 12:49:22 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const form=document.getElementById('login_form');
+const account=document.getElementById('account');
+const password=document.getElementById('password');
+const error=document.getElementById('error');
+
+form.addEventListener('submit',(e)=>{
+    let messages=[]
+    if(account.value===''|| account.value===null){
+        messages.push('Vui lòng nhập tài khoản')
+    }
+    if(password.value===''|| password.value===null){
+        messages.push('Vui lòng nhập mật khẩu')
+    }
+    if(messages.length >0){
+        e.preventDefault()
+        error.innerText=messages[0]
+    }
+})
+// form.addEventListener('submit',(e)=>{
+//     e.preventDefault()
+// })
+
